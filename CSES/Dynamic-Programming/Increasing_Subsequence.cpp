e5eed24c562cd0e1be62825ec0b257da5aef5228
Muztahid-2020331021 <107040850+Muztahid-2020331021@users.noreply.github.com>
2024-03-09 14:10:35 +0600

    Update Increasing_Subsequence.cpp

--- CSES/Dynamic-Programming/Increasing_Subsequence.cpp
+++ CSES/Dynamic-Programming/Increasing_Subsequence.cpp
@@ -29,13 +29,10 @@ void solve(){
     set<int> st;
     for(i=0;i<n;i++){
         auto it = st.lower_bound(v[i]);
-        if(it==st.end()){
-            st.insert(v[i]);
-        }
-        else{
+        if(it!=st.end()){
             st.erase(it);
-            st.insert(v[i]);
         }
+        st.insert(v[i]);
     }
     cout<<st.size()<<endl;
 
