2a36353f0cbcedf09bb9724344b646e717a26b2e
Muztahid-2020331021 <107040850+Muztahid-2020331021@users.noreply.github.com>
2024-03-09 12:10:59 +0600

    Create Increasing_Subsequence.cpp

---
+++ CSES/Dynamic-Programming/Increasing_Subsequence.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll          long long
+#define faster      ios_base::sync_with_stdio(0);
+#define PI          acos(-1.0)
+#define all(x)      x.begin(),x.end()
+#define rall(x)     x.rbegin(),x.rend()
+#define endl        "\n"
+#define fraction(x) fixed<<setprecision(x)
+#define inf         INT_MAX
+
+#define vi      vector<int>
+#define vvi     vector<vi>
+#define vb      vector<bool>
+#define pii     pair<int, int>
+#define vpii    vector<pii>
+#define vvpii   vector<vpii>
+
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    int i;
+    for(i=0;i<n;i++){
+        cin>>v[i];
+    }
+    set<int> st;
+    for(i=0;i<n;i++){
+        auto it = st.lower_bound(v[i]);
+        if(it==st.end()){
+            st.insert(v[i]);
+        }
+        else{
+            st.erase(it);
+            st.insert(v[i]);
+        }
+    }
+    cout<<st.size()<<endl;
+
+}
+
+
+int main()
+{
+    faster
+    solve();
+    
+}
